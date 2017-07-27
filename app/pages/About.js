7aec55cb82f7ec73591dc176c8f98d0aa4fac5e7
linh <linhng15@gmail.com>
2017-07-26 23:10:57 -0500

    fixing the about---still need to add slideshow

--- app/pages/About.js
+++ app/pages/About.js
@@ -4,17 +4,21 @@ import React from "react";
 //public/img/lau.JPG
 
 export default class About extends React.Component {
+
   render() {
     return (
       <div>
         <div class="row">
           <div class="col-lg-12">
             <div class="bio">
-
-              <h2>About Linh Nguyen</h2>
-              <p>Graduated Lake Forest College with a Biology Degree 13'. After all the years in school, I wanted to work. Going into Beautician as a nail technician was to help ease the mind after all the year of doing traditional school: I rather choose physical work for the time of being. Unfortunately, 1yr break equated to 3yrs due to family business struggling for help.</p>
-              <p>Take my chances with <a href="https://codingbootcamp.northwestern.edu/">Northwestern Bootcamp Program</a></p>
-              <p>In this program, I've learn...</p>
+              <h2>A little more about me...</h2>
+              
+              <p>I am hardworker.</p>
+              <p>I am a learner. </p>
+              <p> I don’t need all the answers to be successful, in fact it’s fun to put pieces of puzzles together. I placed myself in a coding boot camp program at <a href="https://codingbootcamp.northwestern.edu/">Northwestern </a> because I want to explores new languages, opening new career windows, and to find myself a team. </p>
+              <p>Admittedly, I’m still at a more experimental attitude. I learned from past occupations, I want stability and a safety net. Therefore, jobs like waitress, bartender, or even being nail technician doesn’t met my expectation on the long run. I learned being a bank teller or a secretary was not enough of a challenge. Anything related to biology was a fun read but it’s not a career choice. I tried working at nursing home, however I enjoyed providing companionship more than the work.  I was definitely too earnest, even so, I love the facts that I already figure out what I want and what I don’t want for my future. </p>
+              <p>Although I’m not the best developer in the field, I have the confidence to do what is necessary to complete my task, my work experience is quite diversify, therefore I’m capable of handling any issues that may arise.  </p>
+              <p>Aside from technology, I enjoys traveling and cooking. </p>
 
             </div>
           </div>
